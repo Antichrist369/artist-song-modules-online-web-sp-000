1adc0fcf7cf52c3ac9c016e6dea2049039d582b8
Anthony Molina <antonio-molina@att.net>
2019-08-20 04:05:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,8 @@
 require 'pry'
+require_relative '../lib/artist.rb'
+require_relative '../lib/song.rb'
+require_relative '../lib/concerns/memorable' 
+require_relative '../lib/concerns/findable'
 
 
 class Song
