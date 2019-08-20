810cc54ed7e7f664f8b89f61e7262a5737ee616e
Anthony Molina <antonio-molina@att.net>
2019-08-20 04:31:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,9 @@ class Song
   
   
   extend Memorable::ClassMethods 
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethodsextend
+  extend Findable::ClassMethods 
+  include Memorable::InstanceMethods 
+  include Paramable::InstanceMethods 
   
   attr_accessor :name
   attr_reader :artist
