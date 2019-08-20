94327c98a2e235347d889cdcc985dbc238ff94ec
Anthony Molina <antonio-molina@att.net>
2019-08-20 05:05:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,7 @@ class Artist
     @@artists = [] 
    def initialize 
      super 
-    @song = [] 
+    @songs = [] 
    end 
    
    def self.all
@@ -25,7 +25,7 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self unless song.artist
+    song.artist = self 
   end
 
   def add_songs(songs)
